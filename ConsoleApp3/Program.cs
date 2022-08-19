72a0ae6828e7901c25c127f429fe38f345a353d9
AndreyPer95 <a.peresypkin2201@gmail.com>
2022-08-19 19:30:32 +0500

    Add list of departments

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,37 +11,59 @@ namespace VacanciesCount
     public class Program
     {
         protected static string url = "https://cz.careers.veeam.com/vacancies";
-        public static IWebDriver driver;
-        public enum Department
-        {
-            AllDepartments,
-            CorporateInformationSystems,
-            QualityAssurance,
-            ResearchDevelopment,
-            TechnicalCustomerSupport,
-            IT,
-            Other,
-            HR,
-            TechnicalDocumentationDevelopment,
-            ProductManagement,
-            Finance,
-            Facilities,
-            Purchasing,
-            Legal
-        }
+
+        
+        //public enum Department
+        //{
+        //    AllDepartments,
+        //    CorporateInformationSystems,
+        //    QualityAssurance,
+        //    ResearchDevelopment,
+        //    TechnicalCustomerSupport,
+        //    IT,
+        //    Other,
+        //    HR,
+        //    TechnicalDocumentationDevelopment,
+        //    ProductManagement,
+        //    Finance,
+        //    Facilities,
+        //    Purchasing,
+        //    Legal
+        //}
+        public static List<string> departments = new List<string>
+            {
+                { "All Departments"},
+                { "Corporate Information Systems"},
+                { "Quality Assurance"},
+                { "Research & Development"},
+            };
         public static void Main(string[] args)
         {
-            string department = "Research & Development";
-            string language = "English";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromSeconds(10));
-            IJavaScriptExecutor Js1 = driver as IJavaScriptExecutor;
-            Js1.ExecuteScript("window.scrollBy(0,300);");
-            SetDeparment(driver, department);
-            SetLanguage(driver, language);
-            Console.WriteLine(GetCountVacancies(driver)); 
+            //string department = "Research & Development";
+            //string language = "English";
+            //var driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl(url);
+            //driver.Manage().Window.Maximize();
+            //driver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromSeconds(10));
+            //IJavaScriptExecutor Js1 = driver; 
+            //Js1.ExecuteScript("window.scrollBy(0,300);");
+            //SetDeparment(driver, department);
+            //SetLanguage(driver, language);
+            //GetCountVacancies(driver); 
+            var departments = new List<string> 
+            {
+                { "All Departments"},
+                { "Corporate Information Systems"},
+                { "Quality Assurance"},
+                { "Research & Development"},
+            };
+
+            var requiredDepartment = Console.ReadLine();
+            int departmentSerialNumber;
+            for (int i = 0; i < departments.Count; i++)
+            {
+                if (departments[i] == requiredDepartment) departmentSerialNumber = i;
+            }
         }
 
         public static int GetCountVacancies(IWebDriver driver)
@@ -51,18 +73,25 @@ namespace VacanciesCount
             return count;
         }
 
-        public static void SetDeparment(IWebDriver driver, string department)
+        public static void SetDeparment(IWebDriver driver)
         {
+            var requiredDepartment = Console.ReadLine();
+            int departmentSerialNumber = 0;
+            for (int i = 0; i < departments.Count; i++)
+            {
+                if (departments[i] == requiredDepartment) departmentSerialNumber = i;
+            }
             var elements = driver.FindElements(By.Id("sl"));
             elements[0].Click();
             driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(15));
             var selectDepartment = driver.FindElement(By.XPath(
-                $"//*[@id=\"root\"]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/a[{((int)Department.ResearchDevelopment)}]"));
+                $"//*[@id=\"root\"]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/a[{departmentSerialNumber}]"));
             selectDepartment.Click();
         }
 
         public static void SetLanguage(IWebDriver driver, string language)
         {
+            
             var elements = driver.FindElements(By.Id("sl"));
             elements[1].Click();
             driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(15));
--- ConsoleApp3/Test.cs
+++ ConsoleApp3/Test.cs
@@ -1,8 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
-
+using System;
 
 namespace VacanciesCount
 {
@@ -11,11 +10,20 @@ namespace VacanciesCount
     {
         [Test]
 
-        [TestCase(12)]
+        [TestCase(13)]
         
         public void AssertCount(int expectedCount)
         {
-            driver = new ChromeDriver();
+            string department = "Research & Development";
+            string language = "English";
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromSeconds(10));
+            IJavaScriptExecutor Js1 = driver;
+            Js1.ExecuteScript("window.scrollBy(0,300);");
+            SetDeparment(driver);
+            SetLanguage(driver, language);
             var actualResult = GetCountVacancies(driver);
             Assert.AreEqual(expectedCount,actualResult);
         }
